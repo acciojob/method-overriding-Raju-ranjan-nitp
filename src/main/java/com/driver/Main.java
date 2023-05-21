775da511bcd65b281b84c1c00239c82c76b5dacb
Raju-ranjan-nitp <raju.ranjan14@gmail.com>
2023-05-21 22:24:55 +0530

    overridden assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+   //task 1
+   public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    //task2
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj = new B();
+        // Task 5
+        obj.meth();
+    }
   
 }
